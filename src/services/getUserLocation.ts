08985bbd59b224521410d685721e5c40bf0246cb
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-02-14 16:41:38 +0100

    problem

--- src/services/getUserLocation.ts
+++ src/services/getUserLocation.ts
@@ -4,22 +4,43 @@ export const getUserLocation = async (): Promise<{
   latitude: number;
   longitude: number;
   location: string;
-} | null> => {
-  try {
-    const res = await axios.get("https://ipapi.co/json/");
+}> => {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject("Geolocation not supported");
+      return;
+    }
 
-    const latitude = res.data.latitude;
-    const longitude = res.data.longitude;
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-    const location =
-      res.data.city ||
-      res.data.region ||
-      res.data.country_name ||
-      "Unknown location";
+        try {
+          const res = await axios.get(
+            "https://nominatim.openstreetmap.org/reverse",
+            {
+              params: {
+                lat: latitude,
+                lon: longitude,
+                format: "json",
+              },
+            },
+          );
 
-    return { latitude, longitude, location };
-  } catch (error) {
-    console.error("Error fetching IP location:", error);
-    return null;
-  }
+          const location =
+            res.data.address.city ||
+            res.data.address.town ||
+            res.data.address.village ||
+            "Unknown location";
+
+          resolve({ latitude, longitude, location });
+        } catch (error) {
+          reject("Error fetching location name");
+          console.log(error);
+        }
+      },
+      () => reject("Permission denied"),
+    );
+  });
 };
