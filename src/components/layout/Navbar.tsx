9261de02e6b1a393c27bd5c38ae673efcb5d4baa
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-02-15 16:55:58 +0100

    p1

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,10 +6,10 @@ import Locationicon from "../icon/Locationicon";
 import Queastionicon from "../icon/Queastionicon";
 import Threelineicon from "../icon/Threelineicon";
 import Refrechbut from "../icon/refrechbut";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Navbar() {
-  const [nameAct, setNameAct] = useState<string>("home");
+  // const [nameAct, setNameAct] = useState<string>("home");
   return (
     <div>
       <div className="navbar hidden sm:flex flex-row w-fit py-3 px-10 gap-8 sm:gap-8 glass-card justify-center items-center rounded-[40px]">
@@ -51,7 +51,7 @@ export default function Navbar() {
       </div>
       <div className="flex sm:hidden glass-card justify-between w-full gap-20 p-2 items-center text-xl font-medium">
         <Threelineicon />
-        <div>{nameAct}</div>
+        <div>home</div>
         <Refrechbut />
       </div>
     </div>
