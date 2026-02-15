bb3a5e541d6b37f0265b4ea10106a0112366be0c
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-02-15 18:59:42 +0100

    phonemode02

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,10 +6,14 @@ import Locationicon from "../icon/Locationicon";
 import Queastionicon from "../icon/Queastionicon";
 import Threelineicon from "../icon/Threelineicon";
 import Refrechbut from "../icon/refrechbut";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function Navbar() {
   // const [nameAct, setNameAct] = useState<string>("home");
+  const [isopen, setopen] = useState<boolean>(false);
+  const handlclick = () => {
+    setopen(!isopen);
+  };
   return (
     <div>
       <div className="navbar hidden sm:flex flex-row w-fit py-3 px-10 gap-8 sm:gap-8 glass-card justify-center items-center rounded-[40px]">
@@ -49,11 +53,53 @@ export default function Navbar() {
           )}
         </NavLink>
       </div>
-      <div className="flex sm:hidden glass-card justify-between w-full gap-20 p-2 items-center text-xl font-medium">
-        <Threelineicon />
+      {/* nanbar phone */}
+      <div className="flex sm:hidden glass-card justify-between w-full gap-22 p-2 items-center text-xl font-medium">
+        <button onClick={handlclick}>
+          <Threelineicon />
+        </button>
         <div>home</div>
         <Refrechbut />
       </div>
+      {/* dachboard */}
+      {isopen && (
+        <div
+          className={` dashboard ${isopen ? "open" : "closed"} flex  sm:hidden flex-col glass-card items-start w-full gap-6 pl-6 pt-5 pb-5 text-xl mt-2 z-50 transition-all duration-500 ease-in-out `}
+        >
+          <NavLink to="/">
+            {({ isActive }) => (
+              <div className={`itme2  ${isActive ? "active" : ""}`}>
+                <Homeicon />
+                <span className="text">home</span>
+              </div>
+            )}
+          </NavLink>
+          <NavLink to="/search">
+            {({ isActive }) => (
+              <div className={`itme2  ${isActive ? "active" : ""}`}>
+                <Searchicon />
+                <span className="text">search</span>
+              </div>
+            )}
+          </NavLink>
+          <NavLink to="/location">
+            {({ isActive }) => (
+              <div className={`itme2  ${isActive ? "active" : ""}`}>
+                <Locationicon />
+                <span className="text">location</span>
+              </div>
+            )}
+          </NavLink>
+          <NavLink to="/question">
+            {({ isActive }) => (
+              <div className={`itme2  ${isActive ? "active" : ""}`}>
+                <Queastionicon />
+                <span className="text">help</span>
+              </div>
+            )}
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 }
